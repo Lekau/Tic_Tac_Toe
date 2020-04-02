6e6df780681b148b1258b35ee3de2e3a46f7302b
Lekau Mamabolo <lekau9222@gmail.com>
2020-04-02 22:19:22 +0200

    plays one round so far, tested and we good.

--- main.c
+++ main.c
@@ -87,17 +87,74 @@ void print_grid(char str[9])
     }
 }
 
-int main(int count, char **players)
+int main(void)
 {
-    int turns = 0;
+    int player2 = 5;
+    int player1 = 0;
+    int turns = 1;
+    char cur_player = '1';
+    char winner[14] = "Player 1 Wins\n";
+    int DQ = 0;
+    char draw_check = 'Y';
+    char playing_grid[9] = "Z12345678";
 
-    while(turns < 9)
+    while(turns <= 9)
     {
+        if(turns % 2 != 0)
+        {
+            cur_player = '1';
+
+            if (disqualifier(playing_grid, player1) == 1)
+            {
+            our_putstr("Player 1, You are disqualified \n");
+            DQ = 1;
+            break;
+            }
+
+            draw_X_or_O(playing_grid, player1, turns);
         
+            if (game_over(playing_grid, turns) == 1)
+                draw_check = 'Y';
+            else
+                draw_check = 'N';
+        }
+        else
+        {
+           cur_player = '2';
+
+            if (disqualifier(playing_grid, player2) == 1)
+            {
+            our_putstr("Player 2, You are disqualified \n");
+            DQ = 1;
+            break;
+            }
+        
+            draw_X_or_O(playing_grid, player2, turns);
+        
+            if (game_over(playing_grid, turns) == 1)
+            {
+                draw_check = 'Y';
+                break;
+            }
+            else if (game_over(playing_grid, turns) == 2)
+            {
+                draw_check = 'N';
+                break;
+            }
+        }
         turns++;
     }
 
+    print_grid(playing_grid);
 
-    print_grid(players[1]);
+    if (draw_check == 'Y' && DQ == 0)
+    {
+        our_putstr("its a Draw Kids \n");
+    } 
+    else if (draw_check == 'N' && DQ == 0)
+    {
+        winner[7] = cur_player;
+        our_putstr(winner);
+    }
     return (0);
 }
