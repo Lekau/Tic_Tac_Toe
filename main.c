ca4ed05e33b650e7b99242d1cebfa7af1b1a4518
Lekau Mamabolo <lekau9222@gmail.com>
2020-04-02 22:48:39 +0200

    Disqualify bug fixed.

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ int game_over(char str[9], int turns)
 
 int disqualifier(char str[9], int pos)
 {
-    if (str[pos] == 'O' || str[pos] == 'X')
+    if (str[pos] == 'O' || str[pos] == 'X' || pos < 0 || pos > 8)
         return (1);
     else
         return (0); 
@@ -89,34 +89,39 @@ void print_grid(char str[9])
 
 int main(void)
 {
-    int player2 = 5;
-    int player1 = 0;
+    int player2 = 9;
+    int player1 = 7;
     int turns = 1;
     char cur_player = '1';
     char winner[14] = "Player 1 Wins\n";
-    int DQ = 0;
     char draw_check = 'Y';
     char playing_grid[9] = "Z12345678";
 
     while(turns <= 9)
     {
-        if(turns % 2 != 0)
+        if(turns%2 != 0)
         {
             cur_player = '1';
 
             if (disqualifier(playing_grid, player1) == 1)
             {
-            our_putstr("Player 1, You are disqualified \n");
-            DQ = 1;
-            break;
+                our_putstr("Player 1, You are disqualified \n");
+                print_grid(playing_grid);
+                return (0);
             }
 
             draw_X_or_O(playing_grid, player1, turns);
         
             if (game_over(playing_grid, turns) == 1)
+            {
                 draw_check = 'Y';
-            else
+                break;
+            }
+            else if (game_over(playing_grid, turns) == 2)
+            {
                 draw_check = 'N';
+                break;
+            }
         }
         else
         {
@@ -124,9 +129,9 @@ int main(void)
 
             if (disqualifier(playing_grid, player2) == 1)
             {
-            our_putstr("Player 2, You are disqualified \n");
-            DQ = 1;
-            break;
+                our_putstr("Player 2, You are disqualified \n");
+                print_grid(playing_grid);
+                return (0);
             }
         
             draw_X_or_O(playing_grid, player2, turns);
@@ -147,11 +152,11 @@ int main(void)
 
     print_grid(playing_grid);
 
-    if (draw_check == 'Y' && DQ == 0)
+    if (draw_check == 'Y')
     {
         our_putstr("its a Draw Kids \n");
     } 
-    else if (draw_check == 'N' && DQ == 0)
+    else if (draw_check == 'N')
     {
         winner[7] = cur_player;
         our_putstr(winner);
