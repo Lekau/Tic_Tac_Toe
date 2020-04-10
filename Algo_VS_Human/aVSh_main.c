d79725ecf14bcb7d843b2a0c27ac20330e98ba75
Lekau Mamabolo <lekau9222@gmail.com>
2020-04-10 17:31:32 +0200

    damn algo keeps getting disqualified...halftime

---
+++ Algo_VS_Human/aVSh_main.c
@@ -0,0 +1,170 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int call_strategy(char grid[9]);
+
+void our_putchar(char k)
+{
+    write(1, &k, 1);
+}
+
+void our_putstr(char *str)
+{
+    int i = 0;
+    while(str[i] != '\0')
+        our_putchar(str[i++]);
+}
+
+void draw_X_or_O(char str[9], int pos, int player)
+{
+    int xo = 0;
+    if (player%2 != 1)
+    {
+        while(xo != pos)
+            xo++;
+        str[xo] = 'X';
+    }
+    else 
+    {
+        while(xo != pos)
+            xo++;
+        str[xo] = 'O';
+    }
+}
+
+int game_over(char str[9], int turns)
+{
+    if((str[0] == str[4] && str[8] == str[4]) || (str[2] == str[4] && str[6] == str[4]))
+    {
+        return (2);
+    }
+    else if ((str[0] == str[3] && str[3] == str[6]) || (str[1] == str[4] && str[4] == str[7]) || (str[2] == str[5] && str[5] == str[8]))
+    {
+        return (2);
+    } 
+    else if ((str[0] == str[1] && str[1] == str[2]) || (str[3] == str[4] && str[4] == str[5]) || (str[6] == str[7] && str[7] == str[8]))
+    {
+        return (2);
+    }
+    else if (turns == 9)
+    {
+        return (1);
+    } 
+    else
+    {
+        return (0);
+    }
+}
+
+int disqualifier(char str[9], int pos)
+{
+    if (str[pos] == 'O' || str[pos] == 'X' || pos < 0 || pos > 8)
+        return (1);
+    else
+        return (0); 
+}
+
+void print_grid(char str[9])
+{
+    int i = 0, k = 0, h_div = 0, v_div = 0;
+
+    while(i < 9)
+    {
+        k = 0;
+        h_div = 0;
+        while(k++ < 3)
+        {
+            if (h_div++ < 2)
+            {
+                our_putchar(str[i++]);
+                our_putstr(" | ");
+            }
+            else
+            {
+                our_putchar(str[i++]);
+                our_putstr(" \n");
+            }
+        }
+        if (v_div++ < 2)
+            our_putstr("----------\n"); 
+    }
+}
+
+int main(void)
+{
+    int player2 = 0;
+    int player1 = 0;
+    int turns = 1;
+    char cur_player = '1';
+    char winner[14] = "Player 1 Wins\n";
+    char draw_check = 'Y';
+    char playing_grid[9] = "Z12345678";
+
+    while(turns <= 9)
+    {
+        if(turns%2 != 0)
+        {
+            cur_player = '1';
+            our_putstr(" \n Your move player one, which position do you wanna play? :");
+            player1 = call_strategy(playing_grid);
+            if (disqualifier(playing_grid, player1) == 1)
+            {
+                our_putstr("\n Player 1, You are disqualified \n");
+                print_grid(playing_grid);
+                return (0);
+            }
+
+            draw_X_or_O(playing_grid, player1, turns);
+        
+            if (game_over(playing_grid, turns) == 1)
+            {
+                draw_check = 'Y';
+                break;
+            }
+            else if (game_over(playing_grid, turns) == 2)
+            {
+                draw_check = 'N';
+                break;
+            }
+        }
+        else
+        {
+            cur_player = '2';
+            our_putstr("\nYour move player two, which position do you wanna play? :");
+            scanf("%d", &player2);
+            if (disqualifier(playing_grid, player2) == 1)
+            {
+                our_putstr(" \n Player 2, You are disqualified \n");
+                print_grid(playing_grid);
+                return (0);
+            }
+        
+            draw_X_or_O(playing_grid, player2, turns);
+        
+            if (game_over(playing_grid, turns) == 1)
+            {
+                draw_check = 'Y';
+                break;
+            }
+            else if (game_over(playing_grid, turns) == 2)
+            {
+                draw_check = 'N';
+                break;
+            }
+        }
+        turns++;
+    }
+
+    print_grid(playing_grid);
+
+    if (draw_check == 'Y')
+    {
+        our_putstr("its a Draw Kids \n");
+    } 
+    else if (draw_check == 'N')
+    {
+        winner[7] = cur_player;
+        our_putstr(winner);
+    }
+    return (0);
+}
--- Algo_VS_Human/algo.c
+++ Algo_VS_Human/algo.c
@@ -1,5 +1,72 @@
+#include <stdio.h>
 #define TRUE 1
 #define FALSE 0
+
+int block_win(char grid[9])
+{       
+        if ((grid[8] != 'X' || grid[8] != 'O') && (grid[0] == 'X' || grid[0] == 'O') && (grid[2] == 'X' || grid[2] == 'O') && (grid[6] == 'X' || grid[6] == 'O'))
+            return (8);
+        else if ( (grid[6] != 'X' || grid[6] != 'O') && (grid[0] == 'X' || grid[0] == 'O') && (grid[2] == 'X' || grid[2] == 'O') && (grid[8] == 'X' || grid[8] == 'O'))
+            return (6);
+        else if ((grid[2] != 'X' || grid[2] != 'O') && (grid[0] == 'X' || grid[0] == 'O') && (grid[6] == 'X' || grid[6] == 'O') && (grid[8] == 'X' || grid[8] == 'O'))
+            return (2);
+        else if ((grid[0] != 'X' || grid[0] != 'O') && (grid[2] == 'X' || grid[2] == 'O') && (grid[6] == 'X' || grid[6] == 'O') && (grid[8] == 'X' || grid[8] == 'O'))
+            return (0);
+        else
+            return (10);
+}
+
+int play(char grid[9], int first_move)
+{
+    int winning_comb[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    int k = 0, i = 0;
+    if (first_move == TRUE)
+    {   
+        printf("first move is true");
+        return (4);
+    }
+    else 
+    {
+        int x_count = 0, o_count = 0;
+        while(k++ <= 7)
+        {
+            x_count = 0;
+            o_count = 0;
+            while(i++ <= 2)
+            {
+                if(grid[winning_comb[k][i]] == 'O')
+                    o_count++;
+                else if (grid[winning_comb[k][i]] == 'X')
+                    x_count++;
+            }
+            if (x_count == 2 || o_count == 2)
+            {
+                if ((grid[winning_comb[k][0]] == 'X' && grid[winning_comb[k][1]] == 'X') || (grid[winning_comb[k][0]] == 'O' && grid[winning_comb[k][1]] == 'O'))
+                    return(winning_comb[k][2]);
+                else if ((grid[winning_comb[k][1]] == 'X' && grid[winning_comb[k][2]] == 'X') || (grid[winning_comb[k][1]] == 'O' && grid[winning_comb[k][2]] == 'O'))
+                    return(winning_comb[k][0]);
+                else 
+                    return(winning_comb[k][1]);
+            }
+            else
+                k++;   
+        }
+        if(block_win(grid) != 10)
+            return (block_win(grid)); 
+        else if(grid[4] != 'O' || grid[4] != 'X')
+            return (4);        
+        else 
+        {
+            int h = 1;
+            while(h <= 7)
+            {
+                if (grid[h] != 'O' || grid[h] != 'X')
+                    return (h);
+                h = h + 2;
+            }
+        }
+    }
+}
 
 int call_strategy(char grid[9])
 {
@@ -16,65 +83,8 @@ int call_strategy(char grid[9])
             count_o++;
         i++;
     }
-    if((count_x == 0 && count_o == 0) || (count_x == count_o))
-    {
         if (count_x == 0 && count_o == 0)
             first_move = TRUE;
-        return (play_offence(grid, first_move));
-    }
-    else
-    {
-        if (count_x == 0 || count_o == 0)
-            first_move = TRUE;
-        return (play_defence(grid, first_move));
-    }
-}
-int play_defence(char grid[9], int first_move)
-{
-    int winning_comb[8][3] = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
-    int k = 0, i = 0;
-    if (first_move == TRUE)
-    {
-        return (4);
-    }
-    else 
-    {
-        int x_count = 0, o_count = 0;
-        while(k++ <= 7)
-        {
-            x_count = 0;
-            o_count = 0;
-            while(i++ <= 2)
-            {
-                if(grid[winning_comb[k][i]] == 'O')
-                    o_count++;
-                else if (grid[winning_comb[k][i]] == 'X')
-                    x_count++;
-
-            }
-            if (x_count == 2 || o_count == 2)
-            {
-                i = 0;
-                //block that shit
-            }
-            else
-                k++;   
-        }
-    }
-}
-int play_offence(char grid[9], int first_move)
-{
-    if (first_move == TRUE)
-    {
-        return (4);
-    }
-
-    //still finding the best way to win this game. 
-}
-
-
-
-int computer_algo(char current_grid[9])
-{
-    return (call_strategy(current_grid));
+            
+        return (play(grid, first_move));
 }
